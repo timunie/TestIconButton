e688d7e6db673ea31540534eff6855aabfb808b7
Jasone <jasone.lenormand@cloud-solutions.fr>
2022-05-18 00:22:54 +0200

    test custom icon

---
+++ TestCustomControl/CustomControls/IconButton.cs
@@ -0,0 +1,22 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCustomControl.CustomControls
+{
+    public class IconButton : Button
+    {
+        public static readonly StyledProperty<string> IconProperty =
+            AvaloniaProperty.Register<IconButton, string>(nameof(Icon));
+
+        public string Icon
+        {
+            get { return GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+    }
+}
---
+++ TestCustomControl/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestCustomControl.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
